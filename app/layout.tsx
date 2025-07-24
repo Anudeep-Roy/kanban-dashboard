06138191c9623f42afa60be9be85a5daf6d6ec37
Anudeep <anudeep.roy@liferay.com>
2025-07-24 14:50:48 +0530

    Style changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href={'/kanban.icon'}></link>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="data-theme"
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,24 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   // Replace with your Firebase config
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAO4g_Xb1ayV4boczzmHX3xOGmLrIiIJEM",
-  authDomain: "kanban-dashoard.firebaseapp.com",
-  projectId: "kanban-dashoard",
-  storageBucket: "kanban-dashoard.firebasestorage.app",
-  messagingSenderId: "815167259965",
-  appId: "1:815167259965:web:495ecccc55368c759c7154",
-  measurementId: "G-1CH0QME4RP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
